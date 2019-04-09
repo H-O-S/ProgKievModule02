a01db1c89b381a76232e95208c605ba38e741ce3
HOS <hos@ukr.net>
2019-04-09 15:58:08 +0300

    Completed task 2 from homework 01

--- src/HW01/Task02/MainHW01Task02.java
+++ src/HW01/Task02/MainHW01Task02.java
@@ -8,4 +8,15 @@
 package HW01.Task02;
 
 public class MainHW01Task02 {
+    public static void main(String[] args) {
+        Triangle triangleOne = new Triangle("One",5, 3, 7);
+        Triangle triangleTwo = new Triangle("Two",4, 8, 6);
+        Triangle triangleThree = new Triangle("Three",10, 2, 9);
+
+        System.out.printf("Triangle area - %.2f%n %s%n", triangleOne.triangleArea(), triangleOne);
+        System.out.printf("Triangle area - %.2f%n %s%n", triangleTwo.triangleArea(), triangleTwo);
+        System.out.printf("Triangle area - %.2f%n %s%n", triangleThree.triangleArea(), triangleThree);
+
+
+    }
 }
--- src/HW01/Task02/Triangle.java
+++ src/HW01/Task02/Triangle.java
@@ -14,4 +14,65 @@
 package HW01.Task02;
 
 public class Triangle {
+    private String name;
+    private double sideOne;
+    private double sideTwo;
+    private double sideThree;
+
+    public Triangle(String name, double sideOne, double sideTwo, double sideThree) {
+        this.name = name;
+        this.sideOne = sideOne;
+        this.sideTwo = sideTwo;
+        this.sideThree = sideThree;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSideOne() {
+        return sideOne;
+    }
+
+    public void setSideOne(double sideOne) {
+        this.sideOne = sideOne;
+    }
+
+    public double getSideTwo() {
+        return sideTwo;
+    }
+
+    public void setSideTwo(double sideTwo) {
+        this.sideTwo = sideTwo;
+    }
+
+    public double getSideThree() {
+        return sideThree;
+    }
+
+    public void setSideThree(double sideThree) {
+        this.sideThree = sideThree;
+    }
+
+    public double triangleArea (){
+        double semiPerimeter = (sideOne + sideTwo + sideThree) / 2;
+        double triangleArea = Math.sqrt(semiPerimeter *
+                (semiPerimeter - sideOne) * (semiPerimeter - sideTwo) + (semiPerimeter - sideThree));
+
+        return triangleArea;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "name='" + name + '\'' +
+                ", sideOne=" + sideOne +
+                ", sideTwo=" + sideTwo +
+                ", sideThree=" + sideThree +
+                '}';
+    }
 }
