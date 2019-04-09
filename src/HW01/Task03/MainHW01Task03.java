f8d7d4f54ea705fdddeacdcafef973ebd84f83f0
HOS <hos@ukr.net>
2019-04-09 18:34:32 +0300

    Completed task 3 from homework 01

--- src/HW01/Task03/MainHW01Task03.java
+++ src/HW01/Task03/MainHW01Task03.java
@@ -2,10 +2,18 @@
  * Java OOP. Home Work 01.
  *
  * @author Oleg Khrulov
- * @version dated 07.04.2019
+ * @version dated 09.04.2019
  */
 
 package HW01.Task03;
 
 public class MainHW01Task03 {
+    public static void main(String[] args) {
+        Vector3d vectorOne = new Vector3d(3.4, 5.2, 7.8);
+        Vector3d vectorTwo = new Vector3d(9.1, 3.7, 1.5);
+
+        System.out.println( "Sum Of Vector: " + Vector3d.sumOfVector(vectorOne, vectorTwo));
+        System.out.println( "Scalar Product Of Vector: " + Vector3d.scalarProductOfVector(vectorOne, vectorTwo));
+        System.out.println( "Vector Product Of Vector: " + Vector3d.vectorProductOfVector(vectorOne, vectorTwo));
+    }
 }
--- src/HW01/Task03/Vector3d.java
+++ src/HW01/Task03/Vector3d.java
@@ -2,7 +2,7 @@
  * Java OOP. Home Work 01.
  *
  * @author Oleg Khrulov
- * @version dated 07.04.2019
+ * @version dated 09.04.2019
  */
 
 /*
@@ -16,4 +16,76 @@
 package HW01.Task03;
 
 public class Vector3d {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3d() {
+    }
+
+    public Vector3d(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector3d {" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+
+    public static Vector3d sumOfVector(Vector3d vectorOne, Vector3d vectorTwo){
+        double x = vectorOne.getX() + vectorTwo.getX();
+        double y = vectorOne.getY() + vectorTwo.getY();
+        double z = vectorOne.getZ() + vectorTwo.getZ();
+
+        return new Vector3d(x, y, z);
+    }
+
+    public static double scalarProductOfVector(Vector3d vectorOne, Vector3d vectorTwo) {
+        double scalarProduct
+                = (vectorOne.getX() * vectorTwo.getX())
+                + (vectorOne.getY() * vectorTwo.getY())
+                + (vectorOne.getZ() * vectorTwo.getZ());
+
+        return scalarProduct;
+    }
+
+    public static Vector3d vectorProductOfVector(Vector3d vectorOne, Vector3d vectorTwo) {
+        Vector3d vectorProductOfVector = new Vector3d(
+                (vectorOne.getY() * vectorTwo.getZ() - vectorOne.getZ() * vectorTwo.getX()),
+                (vectorOne.getZ() * vectorTwo.getX() - vectorOne.getX() * vectorTwo.getZ()),
+                (vectorOne.getZ() * vectorTwo.getX() - vectorOne.getX() * vectorTwo.getZ()));
+
+        return vectorProductOfVector;
+    }
+
 }
